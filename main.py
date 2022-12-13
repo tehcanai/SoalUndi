3281bc0e58eb4009a9ae599bc0efd453e6aca5ab
Arif Zakuan <80711188+tehcanai@users.noreply.github.com>
2022-12-13 23:15:00 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from telegram.ext import filters, MessageHandler, ApplicationBuilder, CallbackCo
 from chatdoc import chatdoc
 import psycopg2
 
-chatbot = chatdoc('Training')
+chatbot = chatdoc('{TRAINING DATASET}')
 
 def write_data(text, file):
     file_obj = open(file, "a", encoding="utf-8")
@@ -25,17 +25,7 @@ async def start(update: Update, context: CallbackContext.DEFAULT_TYPE):
     )
 
 async def echo(update: Update, context: CallbackContext.DEFAULT_TYPE):
-
-    sql = """INSERT INTO public."Questions"("Timestamp", "Questions")
-             VALUES(%s, %s);"""
-
     await context.bot.send_message(chat_id=update.effective_chat.id, text=str(chatbot.generateResponse(update.message.text)))
-    conn_out = psycopg2.connect("dbname=SoalUndiDB user=postgres password=")
-    write_data(update.message.text, "logtest1.txt")
-    cur = conn_out.cursor()
-    cur.execute(sql, (datetime.now(), update.message.text))
-    conn_out.commit()
-    conn_out.close()
 
 if __name__ == '__main__':
     application = ApplicationBuilder().token('5224908027:AAEL_d9wym2Sh291PAN4fVfKK7qkwWjkUqg').build()
